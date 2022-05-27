57fd1c0cbc0ea4f206f00f284d41aa52732936a3
7Brandyn <brandyn.evans7@gmail.com>
2022-05-26 19:57:42 -0700

    Created psudocode for Rock, Paper, Scissors game

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log("Let's do this!!");
+// Rock paper scizzors game psudocode
+
+// Create 3 INPUT buttons
+// INPUT BUTTON=rock
+// INPUT BUTTON=paper
+// INPUT BUTTON=scizzors
+
+// Create Player & CPU variables
+
+// Create a reset button to clear board
+// BUTTON=Reset
+
+// create clear button to clear score
+// BUTTON=clear
+
+// create random CPU generator with the same 3 strings
+// Rock
+// Paper
+// Scizzors
+
+// when INPUT BUTTON is selected, generate RANDOM OUTPUT string
+// Create a rule based off of which INPUT BUTTON you select
+
+// If input=rock
+// lose to OUTPUT paper
+// tie with OUTPUT rock
+// win to OUTPUT scizzors
+
+// If input=paper
+// lose to OUTPUT scizzors
+// tie with OUTPUT paper
+// win to OUTPUT rock
+
+// If input=scizzors
+// lose to OUTPUT rock
+// tie with OUTPUT scizzors
+// win to OUTPUT paper
+
+// Display results on screen
+
+// if player wins, add 1 to PLAYER score
+// if player Loses, add 1 to CUP score
+// if player draws, do nothing
+
+// clicking INPUT=Reset clears the board
+
+// ckicking INPUT=Clear clears score
